c6551ddfa4c458e6bd00e63b4656543d1dce79ff
YunSeung <119654971+loveezis@users.noreply.github.com>
2023-04-22 02:16:56 +0900

    Delete JavaStart directory

--- JavaStart/src/Interface_ex/udgame.java
+++
@@ -1,9 +0,0 @@
-package Interface_ex;
-
-public interface udgame {
-	
-	int ranNumber = (int)Math.ceil(Math.random()*10);
-	public void user();
-	public String result(int ranNumber,int user); 
-	
-}
